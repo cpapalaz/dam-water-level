1e8fdf10004ec6d9f1010be800d45a560c654408
Chloe Papalazarou <cpapalazarou@sparkgeo.com>
2022-07-26 15:46:00 -0700

    created test file for plotting one dataset, added functions to graph.py

--- plotting/graph.py
+++ plotting/graph.py
@@ -1,1 +1,32 @@
 #python function for plotting the data in csv_data
+import os
+import glob
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv   
+
+directoryPath = os.path.join("c:\\","/Users/cpapalaz/dam-water-level/csv_data/")
+
+#print csv files and choose
+
+#extract date range and variable values for the csv file
+for file in glob.glob(directoryPath+"*.csv"):
+    f=open(file, "r")
+    lines = f.readlines()[1:]
+    reader = csv.reader(lines, delimiter=",")
+    date_range =[]
+    var_values = []
+    for row in reader:
+        date_range.append(row[0])
+        var_values.append(row[1])  
+    #plotting
+    plt.plot(date_range, var_values) 
+
+    plt.xlabel("Date")
+    plt.ylabel(file[1])
+
+    plt.xlim()
+    plt.ylim()
+    plt.title(file)
+
+    plt.show()
---
+++ plotting/testgraph.py
@@ -0,0 +1,41 @@
+# test code for graphing one of the csv datasets
+
+import os
+import matplotlib.pyplot as plt
+import pandas as pd
+import csv   
+
+directoryPath = os.path.join("c:\\","/Users/cpapalaz/dam-water-level/csv_data/discharge_powergen_daily.csv")
+
+f=open(directoryPath, "r")
+lines = f.readlines()[1:]
+reader = csv.reader(lines, delimiter=",")
+date_range =[]
+var_values = []
+for row in reader:
+    date_range.append(row[0])
+    var_values.append(row[1])  
+
+date_range.pop()
+var_values.pop()
+
+var_values_int =[]
+for val in var_values:
+    val= int(val.replace(',', ''))
+    var_values_int.append(val)
+
+#plotting
+plt.bar(date_range, var_values_int)
+
+plt.xlabel("Month")
+plt.ylabel("pwr discharge")
+
+plt.title("Discharge Power Daily")
+
+# plt.show()
+
+#table
+data = pd.read_csv(r"/Users/cpapalaz/dam-water-level/csv_data/discharge_powergen_daily.csv")
+df = pd.DataFrame(data)
+
+print (df)
